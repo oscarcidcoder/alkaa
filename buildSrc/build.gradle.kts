a794550ce1ef484dd715b31e2969b89dee9d4882
Igor Escodro <escodro@outlook.com>
2022-02-08 10:30:41 -0300

    ⬆️ Upgrade Build Gradle version
    Build Gradle version updated to latest version matching Android Studio
    Beta

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.2.0-alpha07")
+    implementation("com.android.tools.build:gradle:7.2.0-beta01")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
 
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.19.0")
